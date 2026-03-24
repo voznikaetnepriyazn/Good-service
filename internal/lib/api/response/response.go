58f76904e01e21ec1160913e3c3d2ed9b716afa9
Daria Maslova <maslovadaria16.07@gmail.com>
2026-03-24 20:57:26 +0300

    some responses

--- internal/lib/api/response/response.go
+++ internal/lib/api/response/response.go
@@ -1,1 +1,59 @@
 package response
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Response struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
+func OK() Response {
+	return Response{
+		Status: StatusOK,
+	}
+}
+
+func Error(msg string) Response {
+	return Response{
+		Status: StatusError,
+		Error:  msg,
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is a required field", err.Field()))
+		case "url":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
+		}
+	}
+
+	return Response{
+		Status: StatusError,
+		Error:  strings.Join(errMsgs, ", "),
+	}
+}
+
+func formatValidationError(err validator.ValidationErrors) map[string]string {
+	errors := make(map[string]string)
+	for _, e := range err {
+		errors[e.Field()] = e.Error()
+	}
+	return errors
+}
